862428833e8457d618441131ed19eb44ed7e2d3c
Alifa00 <31669425+Alifa00@users.noreply.github.com>
2021-01-07 21:16:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from math import floor 
-class SimplexTable:
+class STable:
     def init_table(self):
         self.Table=self.copyTable(self.A)
-        self.column_indexes=[]
-        self.raw_indexes=[]
+        self.column_names=[]
+        self.raw_names=[]
         f_table=[0]
         for i in range(len(self.c)):
-            f_table.append(-self.c[i] if self.to_min else self.c[i])
+            f_table.append(-self.c[i] if self.orientation else self.c[i])
         self.Table.append(f_table)
         for i in range(len(self.b)):
             self.Table[i].insert(0,self.b[i])
-        self.column_indexes=["S0"]
+        self.column_names=["S0"]
         for i in range(len(self.c)):
-            self.column_indexes.append("x"+str(i+1))
+            self.column_names.append("x"+str(i+1))
         for i in range(len(self.b)):
-            self.raw_indexes.append("x"+str(len(self.c)+i+1))
-        self.raw_indexes.append("F")
+            self.raw_names.append("x"+str(len(self.c)+i+1))
+        self.raw_names.append("F")
         print("F=",end="")
         for i in range(len(self.c)):
-             print(self.c[i],"*",self.column_indexes[i+1],"+",end="",sep="")
+             print(self.c[i],"*",self.column_names[i+1],"+",end="",sep="")
         print("0",end='');
-        print(" => ", "min" if self.to_min else "max")
+        print(" => ", "min" if self.orientation else "max")
         
     def update_table(self,newTable,newB,newC,newTomin):
-        self.to_min=newTomin 
+        self.orientation=newTomin 
         self.b=self.copyList(newB)
         self.c=self.copyList(newC)
         self.A=self.copyTable(newTable)
@@ -34,18 +34,18 @@ class SimplexTable:
         self.cannon_form=[]
         self.Table=[]
         self.A=[]
-        self.column_indexes=[]
-        self.raw_indexes=[]
-        self.to_min=True
+        self.column_names=[]
+        self.raw_names=[]
+        self.orientation=True
     def init_from_file(self,f,dvoy=False):
         dir_line_no_split=f.readline()
         while(dir_line_no_split=='\n'):
                 dir_line_no_split=f.readline()
         dir_line=dir_line_no_split.split()
         if(dir_line[0]=="max"):
-            self.to_min=False
+            self.orientation=False
         if(dir_line[0]=="min"):
-            self.to_min=True
+            self.orientation=True
         c_line=f.readline();
         while(c_line=='\n'):
                 c_line=f.readline()
@@ -66,7 +66,7 @@ class SimplexTable:
             self.c=self.copyList(temp)
             temp=self.copyTable(self.A)
             self.A=self.Transport(temp,minus=True)
-            self.to_min=not self.to_min
+            self.orientation=not self.orientation
         self.init_table()
     def copyTable(self,_Table, minus=False):
        result=[]
@@ -102,40 +102,37 @@ class SimplexTable:
        return result
     def printTable(self):
         print("      |",end="");
-        for i in self.column_indexes:
+        for i in self.column_names:
             print("%-6s"%i+"|",end="")
         print("")
-        for i in range(len(self.column_indexes)+1):
+        for i in range(len(self.column_names)+1):
             print("%-6s"%"------"+"|",end="")
         print("")
-        for i in range(len(self.raw_indexes)):
-            for j in range(len(self.column_indexes)+1):
+        for i in range(len(self.raw_names)):
+            for j in range(len(self.column_names)+1):
                 if(j==0):
-                    print("%-6s"%self.raw_indexes[i]+"|",end="")
+                    print("%-6s"%self.raw_names[i]+"|",end="")
                 else:
                     print("%-6s"%str(round(self.Table[i][j-1],2))+"|",end="")
             print("")
-            for i in range(len(self.column_indexes)+1):
+            for i in range(len(self.column_names)+1):
                 print("%-6s"%"------"+"|",end="")
             print("")  
     def jardan(self,r,k):
         newTable=self.copyTable(self.Table)
         srk=self.Table[r][k]
-        for i in range(len(self.raw_indexes)):
-            for j in range(len(self.column_indexes)):
+        for i in range(len(self.raw_names)):
+            for j in range(len(self.column_names)):
                 newTable[i][j]=self.Table[i][j]-self.Table[i][k]*self.Table[r][j]/srk
-                
-        for j in range(len(self.column_indexes)):
+        for j in range(len(self.column_names)):
             newTable[r][j]=self.Table[r][j]/srk
-            
-        for j in range(len(self.raw_indexes)):
+        for j in range(len(self.raw_names)):
             newTable[j][k]=-self.Table[j][k]/srk
-            
         newTable[r][k]=1/srk
         
-        tmp=self.raw_indexes[r]
-        self.raw_indexes[r]=self.column_indexes[k]
-        self.column_indexes[k]=tmp
+        tmp=self.raw_names[r]
+        self.raw_names[r]=self.column_names[k]
+        self.column_names[k]=tmp
         self.Table=self.copyTable(newTable)
     def isOpornoe(self):
         for i in range(len(self.Table)-1):
@@ -171,7 +168,7 @@ class SimplexTable:
                     return min_ind,i
                     
             return -1,-1
-    def doSimplex(self):
+    def get_simpl_result(self):
         self.printTable()
         print("")
         Opor=self.isOpornoe()
@@ -196,46 +193,46 @@ class SimplexTable:
           Optim=self.isOptimal()
         self.printTable()
         print("Найдено оптимальное решение")
-        result=self.Table[len(self.Table)-1][0]if self.to_min else -self.Table[len(self.Table)-1][0]
+        result=self.Table[len(self.Table)-1][0]if self.orientation else -self.Table[len(self.Table)-1][0]
         print("F=",round(result,7))
         isInteger=True
         notInteger=0
         LineNum=0
         Value=0
-        for h in range(len(self.raw_indexes)-1):
-            if(getIntegerIndex(self.raw_indexes[h])<=len(self.c)):
+        for h in range(len(self.raw_names)-1):
+            if(getIntegerIndex(self.raw_names[h])<=len(self.c)):
                if not int(round(self.Table[h][0],7))==float(round(self.Table[h][0],7)) and abs(round(self.Table[h][0],7)-floor(round(self.Table[h][0],7)))>Value-floor(Value):
                   isInteger=False
-                  notInteger=self.raw_indexes[h]
+                  notInteger=self.raw_names[h]
                   Value=round(self.Table[h][0],7)
-               print(self.raw_indexes[h],"=",round(self.Table[h][0],7),end=" ")
-        for h in range(1,len(self.column_indexes),1):
-          if(getIntegerIndex(self.column_indexes[h])<=len(self.c)):
-               print(self.column_indexes[h],"=0",end=" ")
+               print(self.raw_names[h],"=",round(self.Table[h][0],7),end=" ")
+        for h in range(1,len(self.column_names),1):
+          if(getIntegerIndex(self.column_names[h])<=len(self.c)):
+               print(self.column_names[h],"=0",end=" ")
         print("")
         return result,isInteger,notInteger,Value
     def printAnswer(self):
         print("Ответ:")
-        result=self.Table[len(self.Table)-1][0]if self.to_min else -self.Table[len(self.Table)-1][0]
+        result=self.Table[len(self.Table)-1][0]if self.orientation else -self.Table[len(self.Table)-1][0]
         print("F=",round(result,13))
-        for h in range(len(self.raw_indexes)-1):
-            if(getIntegerIndex(self.raw_indexes[h])<=len(self.c)):
-               print(self.raw_indexes[h],"=",round(self.Table[h][0],7),end=" ")
-        for h in range(1,len(self.column_indexes),1):
-          if(getIntegerIndex(self.column_indexes[h])<=len(self.c)):
-               print(self.column_indexes[h],"=0",end=" ")
+        for h in range(len(self.raw_names)-1):
+            if(getIntegerIndex(self.raw_names[h])<=len(self.c)):
+               print(self.raw_names[h],"=",round(self.Table[h][0],7),end=" ")
+        for h in range(1,len(self.column_names),1):
+          if(getIntegerIndex(self.column_names[h])<=len(self.c)):
+               print(self.column_names[h],"=0",end=" ")
         print("")
     def Gomori(self,notInteger):
         raw_ind=0
-        for h in range(len(self.raw_indexes)-1):
-            if notInteger==self.raw_indexes[h]:
+        for h in range(len(self.raw_names)-1):
+            if notInteger==self.raw_names[h]:
                   raw_ind=h
         newLine=[]
-        for i in range(len(self.column_indexes)):
+        for i in range(len(self.column_names)):
             newLine.append(-(self.Table[raw_ind][i]-floor(self.Table[raw_ind][i])))
-        self.Table.insert(len(self.raw_indexes)-1,newLine)
+        self.Table.insert(len(self.raw_names)-1,newLine)
        
-        self.raw_indexes.insert(len(self.raw_indexes)-1,"x"+str(len(self.raw_indexes)+len(self.column_indexes)-1))
+        self.raw_names.insert(len(self.raw_names)-1,"x"+str(len(self.raw_names)+len(self.column_names)-1))
     
         
 
@@ -246,12 +243,12 @@ def getIntegerIndex(notInteger):
     return result
 
 f=open("input.txt",'r')
-ST=SimplexTable()
+ST=STable()
 ST.init_from_file(f)
 
-result,isInteger,notInteger,Value=ST.doSimplex()
+result,isInteger,notInteger,Value=ST.get_simpl_result()
 while not isInteger and result!=None:
    ST.Gomori(notInteger)
-   result,isInteger,notInteger,Value=ST.doSimplex()
+   result,isInteger,notInteger,Value=ST.get_simpl_result()
 ST.printAnswer()
 
